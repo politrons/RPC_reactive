bc189ce123155e52bc895e3e7d3833f9284ea4d9
ZH11 <Pablo.Garcia@uk.tesco.com>
2018-02-11 12:29:24 +0100

    documentation

--- src/main/java/com/politrons/grpc/reactive/ReactiveServiceImpl.java
+++ src/main/java/com/politrons/grpc/reactive/ReactiveServiceImpl.java
@@ -3,8 +3,12 @@ package com.politrons.grpc.reactive;
 import io.grpc.stub.StreamObserver;
 
 /**
- * From the server side we will get the entry stream which is the response and we will return a new stream with the request
- * to be used from the client to start the communication.
+ * From the server side we will receive the entry stream which is the response type class [ReactiveResponse]
+ * and we will return a new stream with the request type class [ReactiveRequest] that contains the response Stream.
+ *
+ * Now that client and server side share the [StreamObserver<ReactiveRequest>] the communication can start.
+ * All client actions agains the callbacks [onNext][onError][onComplete] will use the response stream to response
+ * the client and keep communication open, until one of the side decide to close the communication invoking onCompleted.
  */
 public class ReactiveServiceImpl extends ReactiveServiceGrpc.ReactiveServiceImplBase {
 
